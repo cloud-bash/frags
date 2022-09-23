46311db702d081a1fdd9c2e189977508532dfb36
sysad.sacks <sysad.sacks@gmail.com>
2022-09-23 14:01:39 -0400

    random breakthrough with opacity

--- sketch.js
+++ sketch.js
@@ -1,17 +1,30 @@
 function setup() {
-  createCanvas(600, 600);
+  frameRate(10)
+  createCanvas(900, 900);
 }
 
 function draw() {
-  background(220);
-    //creates a grid of squares
-  for(i=0;i<9;i++){
-    x=(width/9)*i
-    for(j=0;j<9;j++){
-      y=(height/9)*j
+   // creates a grid of squares
+  squares(5);
+}
+
+function squares(n){
+  q=random(255);
+  r=random(255);
+  s=random(255);
+  background(q,r,s)
+  for(i=0;i<n;i++){
+    x=(width/n)*i
+    for(j=0;j<n;j++){
+      a=random(255);
+      b=random(255);
+      c=random(255);
+      d=random(255);
+      y=(height/n)*j
       //TODO: load images and get values for grid
-      fill((255/9)*i,(255/18)*(i+j),(255/9)*j)
-      rect(x,y,width/9,height/9);
+      noStroke();
+      fill((a/(n/2))*j,(b/(n*2))*(i-j),(c/(n/2))*i,d)
+      rect(x,y,width/n,height/n);
     }
   }
 }
